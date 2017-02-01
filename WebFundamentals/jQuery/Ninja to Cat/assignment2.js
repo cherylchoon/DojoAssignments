490fa0c7414219fc339c1311c679b8549ae1bb3f
choon2 <cherylchoon@Cheryls-MacBook.local>
2017-02-01 14:57:54 -0600

    group assignment

--- WebFundamentals/jQuery/Ninja to Cat/assignment2.js
+++ WebFundamentals/jQuery/Ninja to Cat/assignment2.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  $('img').click(function() {
+  $('img').hover(function() {
     var currentImage = $(this).attr('src');
     console.log('data-alt-src value is', $(this).attr('data-alt-src'));
     $(this).attr('src',$(this).attr('data-alt-src'));
