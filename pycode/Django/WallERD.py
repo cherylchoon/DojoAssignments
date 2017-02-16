7be87fbc7461a151bd1bcb81ba4581a395879139
cherylchoon <cherylchoon@gmail.com>
2017-02-16 14:23:27 -0600

    Delete WallERD.py

--- pycode/Django/WallERD.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Users(models.Model):
-    first_name = models.Charfield(max_length=38)
-    last_name = models.Charfield(max_length=38)
-    email = models.Charfield(max_length=50)
-    password = models.Charfield(max_length=38)
-    created_at = models.DateTimeField(auto_now_add)
-    updated_at = models.DateTimeField(auto_now_add)
